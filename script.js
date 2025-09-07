055fe3e16cba773cc0a7a373b4f72405e3d6aafc
LimonArdent <santosugalde@comunidad.unam.mx>
2025-09-07 15:14:38 -0600

    subir proyecto

---
+++ script.js
@@ -0,0 +1,16 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach
+(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', () => {
+            const navbar = document.querySelector('.navbar');
+            navbar.style.backgroundColor = window.scrollY > 50 ? 
+                'rgba(10, 10, 10, 0.98)' : 'rgba(10, 10, 10, 0.95)';
+        });
